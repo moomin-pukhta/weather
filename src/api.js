813a9b98ad6a3706e8874eea036eec4aa107c1f7
moomin-pukhta <m0minpukhta@gmail.com>
2024-04-09 15:18:00 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "b0be02297amsh603a3b276cd141ep130b67jsne748e695fd4c",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "6d3911e12dacd53833da91705e3b58af"; 
+  
